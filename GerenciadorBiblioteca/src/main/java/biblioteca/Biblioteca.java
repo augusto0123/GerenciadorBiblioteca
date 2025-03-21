b384ee1f1e79d4081f24087a8db45d356e6a4d2c
Augusto <senescal.oliveira@gmail.com>
2025-03-21 10:30:50 -0300

    feat: adicionado lista de livros

--- GerenciadorBiblioteca/src/main/java/biblioteca/Biblioteca.java
+++ GerenciadorBiblioteca/src/main/java/biblioteca/Biblioteca.java
@@ -1,4 +1,11 @@
 package biblioteca;
 
+import model.Livro;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Biblioteca {
+
+    private List<Livro> livros = new ArrayList<>();
 }
